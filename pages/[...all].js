ffd4c1d58ca7412d126eb95a3ecef44c9ad195e0
sjmog <s_morgan@me.com>
2021-02-20 17:58:31 +0000

    Initial commit with a couple of colours reflecting the URL

---
+++ pages/[...all].js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import {withRouter} from 'next/router'
+
+const Home = ({ router }) => {
+
+  console.log(router.asPath)
+
+  const colors = router.asPath.split('/').filter(({length}) => length > 0)
+
+  return (
+    <div>
+      <Head>
+        <title>Color</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        { colors.map((color, index) => <div className="Color" key={`color-${index}`} style={{ backgroundColor: `#${ color }`, width: `${100.0 / colors.length}vw`}} />) }
+      </main>
+    </div>
+  )
+}
+
+export default withRouter(Home)
